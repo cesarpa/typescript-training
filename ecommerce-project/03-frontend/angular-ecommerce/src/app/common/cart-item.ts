3c529459d3311c588a7704c91fa4094aab66feb2
Cesarpa Code <cesar.1023@hotmail.com>
2023-06-13 14:44:25 -0500

    Adding cartitem component

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/common/cart-item.ts
@@ -0,0 +1,22 @@
+import { provideRoutes } from "@angular/router";
+import { Product } from "./product";
+
+export class CartItem {
+
+    id?: string;
+    name?: string;
+    imageURL?: string;
+    
+    unitPrice?: number;
+    quantity?: number;
+
+    constructor(product: Product){
+        this.id = product.id;
+        this.name = product.name;
+        this.imageURL = product.imageUrl;
+        this.unitPrice = product.unitPrice;
+
+        this.quantity = 1;
+
+    }
+}
