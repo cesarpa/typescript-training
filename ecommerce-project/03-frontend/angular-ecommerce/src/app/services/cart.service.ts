e55c0d25387e9315d03df1645955baa27491c5fe
Cesarpa Code <cesar.1023@hotmail.com>
2023-06-27 16:05:00 -0500

    adding cart service

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>(); 
+  totalQuantity: Subject<number> = new Subject<number>(); 
+
+  constructor() { }
+
+  addToCart(theCartItem : CartItem){
+
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined!;
+
+    if(this.cartItems.length > 0){
+      for(let tempCartItem of this.cartItems){
+        if(tempCartItem.id == theCartItem.id){
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+
+    if(alreadyExistsInCart){
+      existingCartItem.quantity= existingCartItem.quantity!+1;
+    }else{
+      this.cartItems.push(theCartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  
+  computeCartTotals(){
+      
+  }
+  
+}
