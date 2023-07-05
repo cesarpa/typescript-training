b1cc66a181b0ff1ca222a4ffeaefb6796f5994d1
Cesarpa Code <cesar.1023@hotmail.com>
2023-07-05 17:35:47 -0500

    compute the totals also consoling log some data

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -40,7 +40,31 @@ export class CartService {
 
   
   computeCartTotals(){
-      
+      let totalPriceValue: number =0;
+      let totalQuantityValue: number =0;
+
+
+      for (let currentCartItem of this.cartItems){
+        totalPriceValue+= currentCartItem.quantity! * currentCartItem.unitPrice!;
+        totalQuantityValue+= currentCartItem.quantity!;
+      }
+
+      //publish the new values
+
+      this.totalPrice.next(totalPriceValue);
+      this.totalQuantity.next(totalQuantityValue);
+
+      this.logCartData(totalPriceValue, totalQuantityValue)
   }
   
+
+  logCartData(totalPriceValue: number, totalQuantityValue:number){
+    console.log('Content of the cart')
+    for (let tempCartItem of this.cartItems){
+      console.log(tempCartItem);
+    }
+
+    console.log('total price is'+totalPriceValue);
+
+  }
 }
